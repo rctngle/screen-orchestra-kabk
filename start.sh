d3ec7a29733a554a2f928d792fc2e3f4f20d2049
kabkopenday2020 <60257343+kabkopenday2020@users.noreply.github.com>
2020-01-24 15:23:44 +0100

    start script update

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@ osascript -e "set Volume 0"
 \/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --autoplay-policy=no-user-gesture-required
 cd "$(dirname "$0")"
 pwd
-open "$(dirname -- "$0")/monitor.html"
+#open "$(dirname -- "$0")/monitor.html"
+
+script_name=$0
+script_full_path=$(dirname "$0")
+
+open "$script_full_path/monitor.html"
