36079cfedfca8d48996e40f4ab8e656579b9b58c
Daniel Powers <dancepowers@gmail.com>
2020-01-24 15:09:16 +0100

    start script update

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 osascript -e "set Volume 0"
 \/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --autoplay-policy=no-user-gesture-required
+cd "$(dirname "$0")"
+pwd
 open "$(dirname -- "$0")/monitor.html"
